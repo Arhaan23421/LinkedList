2a1b638b9c4f5b05352056e2a2e413e269339319
Arhaan Gupta-Rastogi <119004196+Arhaan23421@users.noreply.github.com>
2024-08-12 20:55:49 +0000

    organized the code

--- main.cpp
+++ main.cpp
@@ -8,10 +8,24 @@ class Node
 public:
 char data;
 Node* next;
+
+int length()
+{
+  int counter = 0;
+  Node* sample = this;
+  while(sample)
+  {
+    counter++;
+    sample = sample->next;
+  }
+  return counter;
+}
+
 private:
 };
 
 void printList(Node* sample);
+int length(Node* sample);
 
 int main()
 {
@@ -25,7 +39,7 @@ int main()
   sampleA->next = sampleB;
   sampleB->next = sampleC;
 
-  
+  cout << "This is a list of " << sampleA->length() << " elements:" << endl;
   printList(sampleA); // [A, B, C]
   return 0;
 }
@@ -38,3 +52,4 @@ void printList(Node* sample)
     sample =sample->next;
   }
 }
+
