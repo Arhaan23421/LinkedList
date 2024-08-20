4ec0239f093ef409a7c57e9098f9db08f96497bf
Arhaan Gupta-Rastogi <119004196+Arhaan23421@users.noreply.github.com>
2024-08-20 19:57:48 +0000

    Added a couple methods and improved others.

--- main.cpp
+++ main.cpp
@@ -4,34 +4,13 @@
 
 using namespace std;
 
-class Node
-{
-public:
-char data;
-Node* next;
-
-int length()
-{
-  int counter = 0;
-  Node* sample = this;
-  while(sample)
-  {
-    counter++;
-    sample = sample->next;
-  }
-  return counter;
-}
- // private:
-
-};
-
 class List
 {
   public:
-  int length;
 
   void add(char data)
   {
+    this->length +=1;
     if(head== nullptr)
     {
       Node* tempres = new Node();
@@ -48,6 +27,7 @@ class List
     temp->next = result;
     result->data = data;
   }
+
   void print()
   {
     cout << "[";
@@ -65,6 +45,7 @@ class List
 
   void insert( char datas, int index)
   {
+    this->length+=1;
     Node* temp = head;
     Node* news = new Node();
     news->data = datas;
@@ -87,6 +68,7 @@ class List
 
   void del( int index)
   {
+    this->length-=1;
     Node* temp = head;
     if(index==0)
     {
@@ -105,10 +87,64 @@ class List
     Node* temp2 = temp3->next->next;
     delete temp3->next;
     temp3->next = temp2;
+
+  }
+
+  int lengthret()
+  {
+    return this->length;
+  }
+
+  char get(int index)
+  {
+    int counter = 0;
+    Node* temp3 = head;
+    while(counter != index)
+    {
+      temp3 = temp3->next;
+      counter++;
+    }
+    return temp3->data;
+  }
+
+  int indexOf(char sample)
+  {
+    int counter=0;
+    Node* temp3 = head;
+    while(sample != temp3->data)
+    {
+      temp3 = temp3->next;
+      counter++;
+      if(counter == this->length)
+      {
+        return -1;
+      }
+    }
+    return counter;
+  }
+
+  bool contains(char sample)
+  {
+    return indexOf(sample) > -1;
+  }
+
+  void clear()
+  {
+    while (this->length)
+    {
+      this->del(0);
+    }
   }
 
   private:
+  struct Node
+  {
+  public:
+    char data;
+    Node* next;
+  };
   Node* head;
+  int length;
 };
 
 
@@ -125,7 +161,22 @@ int main()
   myList->print();
   myList->del(1);
   myList->print();
-  
+  cout << "There are " << myList->lengthret() << " elements in the list." << endl;
+  cout << "Element at 0: " << myList->get(0) << endl;
+  cout << "Element at 1: " << myList->get(1) << endl;
+  cout << "Element at 2: " << myList->get(2) << endl;
+  cout << "Index of 'C': " << myList->indexOf('C') << endl;
+  cout << "Index of 'B': " << myList->indexOf('B') << endl;
+  cout << "Index of 'D': " << myList->indexOf('D') << endl;
+  cout << "Index of 'A': " << myList->indexOf('A') << endl;
+  cout << "Contains 'B': " << myList->contains('B') << endl;
+  cout << "Contains 'A': " << myList->contains('A') << endl;
+  cout << "Before clear: ";
+  myList->print();
+  myList->clear();
+  cout << "After clear: ";
+  myList->print();
+  delete myList;
   return 0;
 }
 
